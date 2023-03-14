91c50ec70ac9c636cbb26d7a1abba2d3c67394d6
warmzhe <1024745625@qq.com>
2023-03-14 15:02:33 +0800

    test3

--- src/com/chen/HelloWorld.java
+++ src/com/chen/HelloWorld.java
@@ -5,7 +5,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hi, Hello World.");
         String str= "Hello,大家好，这是第一个push的程序";
-
         System.out.println(str);
     }
 
